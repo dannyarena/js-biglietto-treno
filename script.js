b972432da801116527bbc8349f333042aad46c96
dannyarena <arenadanny71@gmail.com>
2025-04-01 15:48:01 +0200

    implement final ticket price calculation with age-based discounts

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ const prezzoBase = km * 0.21;
 
 
 console.log("Prezzo base del biglietto:", prezzoBase);
+
+let prezzoFinale = prezzoBase; // per adesso è uguale al prezzo base
+
+//condizioni
+if (etaNumero < 18) {
+  // Sconto del 20% per i minorenni
+  prezzoFinale = prezzoBase * 0.8;
+} else if (etaNumero > 65) {
+  // Sconto del 40% per gli over 65
+  prezzoFinale = prezzoBase * 0.6;
+}
+
+// Formattiamo il prezzo finale con massimo due decimali
+prezzoFinale = prezzoFinale.toFixed(2);
+
+// Mostriamo il prezzo finale
+console.log("Prezzo finale del biglietto: €", prezzoFinale);
