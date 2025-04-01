83f9d0a6c730a26af3445a38576f754915e47e69
dannyarena <arenadanny71@gmail.com>
2025-04-01 15:34:46 +0200

    add script.js and link it in index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const chilometri = prompt("Quanti chilometri vuoi percorrere?");
+
+
+const eta = prompt("Quanti anni hai?");
+
+
+console.log("Chilometri inseriti:", chilometri);
+console.log("Età inserita:", eta);
