21dfb6b9194720817391217fb045b6b543480fe5
dannyarena <arenadanny71@gmail.com>
2025-04-01 15:44:21 +0200

    calculate ticket price based on kilometers and age input

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const eta = prompt("Quanti anni hai?");
 
 console.log("Chilometri inseriti:", chilometri);
 console.log("Età inserita:", eta);
+
+
+const km = Number(chilometri);
+const etaNumero = Number(eta);
+
+
+const prezzoBase = km * 0.21;
+
+
+console.log("Prezzo base del biglietto:", prezzoBase);
